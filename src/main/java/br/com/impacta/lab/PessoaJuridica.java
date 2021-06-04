5eaa69daae8d409996a53f316eac8caf3b61e54b
Eduhkbr <edu.abreu09@hotmail.com>
2021-06-04 18:43:30 -0300

    Criacao das duas classes com implementacao da classe Pessoa

---
+++ src/main/java/br/com/impacta/lab/PessoaJuridica.java
@@ -0,0 +1,14 @@
+package br.com.impacta.lab;
+
+public class PessoaJuridica implements Pessoa{
+
+    @Override
+    public String falarDocumento() {
+        return null;
+    }
+
+    @Override
+    public String getTipo() {
+        return null;
+    }
+}
