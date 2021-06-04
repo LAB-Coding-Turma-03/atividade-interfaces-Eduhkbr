adb074a0e0d7ec615dd3d1c6edd96648cc739afe
Eduhkbr <edu.abreu09@hotmail.com>
2021-06-04 18:48:59 -0300

    Implementação dos metodos

--- src/main/java/br/com/impacta/lab/PessoaFisica.java
+++ src/main/java/br/com/impacta/lab/PessoaFisica.java
@@ -2,13 +2,21 @@ package br.com.impacta.lab;
 
 public class PessoaFisica implements Pessoa{
 
+    private String documento;
+    private String tipo;
+
+    public PessoaFisica(String documento, String tipo){
+        this.documento = documento;
+        this.tipo = tipo;
+    };
+
     @Override
     public String falarDocumento() {
-        return null;
+        return documento;
     }
 
     @Override
     public String getTipo() {
-        return null;
+        return tipo;
     }
 }
